ed72701ec34778a872bf9a4ee3fa65f773bdc415
小呆呆666 <xdd666t@gmail.com>
2023-06-04 23:32:50 +0800

    commit

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/xdd666/Develop/SDK/flutter_sdk"
+export "FLUTTER_APPLICATION_PATH=/Users/xdd666/Develop/Github/flutter_smart_dialog"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=4.9.0"
+export "FLUTTER_BUILD_NUMBER=6"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
