2ed9b0eac719ba81fc7ad41f999e9e67765db7b9
小呆呆666 <xdd666@foxmail.com>
2024-05-27 20:34:31 +0800

    add issue191_toast

---
+++ example/lib/demo/issue191_toast.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () => _show(),
+            child: const Text('show'),
+          ),
+        ),
+      ),
+      navigatorObservers: [FlutterSmartDialog.observer],
+      builder: FlutterSmartDialog.init(),
+    );
+  }
+
+  void _show() async {
+    SmartDialog.showLoading();
+    await Future.delayed(const Duration(seconds: 1));
+    await SmartDialog.dismiss(status: SmartStatus.loading);
+    SmartDialog.showToast('test toast');
+  }
+}
